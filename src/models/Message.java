877528c2dd8439db2b504aed49e0519ba39334c4
yamamoto.N2206 <yamamoto.N2206@ESPE201286.hit-inc.jp>
2022-04-28 15:55:48 +0900

    NewServlet.javaを作成

--- src/models/Message.java
+++ src/models/Message.java
@@ -34,8 +34,8 @@ public class Message {
     @Column(name="created_at", length=255, nullable=false)
     private Timestamp created_at;
 
-    @Column(name="update_at", length=255, nullable=false)
-    private Timestamp update_at;
+    @Column(name="updated_at", length=255, nullable=false)
+    private Timestamp updated_at;
 
     public Integer getId() {
         return id;
@@ -69,11 +69,13 @@ public class Message {
         this.created_at = created_at;
     }
 
-    public Timestamp getUpdate_at() {
-        return update_at;
+    public Timestamp getUpdated_at() {
+        return updated_at;
     }
 
-    public void setUpdate_at(Timestamp update_at) {
-        this.update_at = update_at;
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
+
+
 }
